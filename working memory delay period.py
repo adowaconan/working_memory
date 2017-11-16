0875c104db7225141d9ed5e03b18dcf007cbe778
Ning Mei <semsociaty@gmail.com>
2017-11-15 19:04:53 -0500

    back to square one

--- working memory delay period.py
+++ working memory delay period.py
@@ -33,7 +33,7 @@ def delay_onset(x,key='71'):
         return False
 def make_clf():
     clf = []
-#    clf.append(('vectorizer',Vectorizer()))
+    #clf.append(('vectorizer',Vectorizer()))
     clf.append(('scaler',StandardScaler()))
     estimator = SVC(kernel='linear',max_iter=int(-1),random_state=12345,class_weight='balanced')
     estimator = LinearModel(estimator)
@@ -43,6 +43,99 @@ def make_clf():
 os.chdir('D:\\working_memory\\')
 subs = np.arange(11,33)
 subs = np.setdiff1d(subs,np.array([24,27]))
+
+
+from tqdm import tqdm
+from sklearn import metrics
+import pickle
+n_ = 50
+for sub in subs:    
+    epoch_fif = [f for f in os.listdir() if ('-epo.fif' in f) and ('suj%d_'%(sub) in f)]    
+    epochs_1 = mne.read_epochs(epoch_fif[0])
+    epochs_2 = mne.read_epochs(epoch_fif[1])
+    epochs = mne.concatenate_epochs([epochs_1,epochs_2])
+    epochs.event_id = {'load_2':2,'load_5':5}
+    epochs.pick_types(eeg=True,eog=False)
+    
+    labels = np.array(epochs.events[:,-1] == 2,dtype=int)
+    data = epochs.get_data()
+    times = epochs.times
+    info = epochs.info
+    del epochs
+    
+    cv = KFold(n_splits=4,shuffle=True,random_state=12345)
+    size = len(times[::n_])
+    scores=np.zeros((size,size,4))
+    clfs = []
+    print('cv diagonal\n')
+    for idx_train,_ in tqdm(enumerate(times[::n_]),desc='diag loop'):
+        scores_ = []
+        clfs_ = []
+        for train_,test_ in cv.split(data):
+            clf = make_clf()
+            clf.fit(data[train_,:,idx_train],labels[train_])
+            clfs_.append(clf)
+            scores_.append(metrics.roc_auc_score(labels[test_],
+                                    clf.predict(data[test_,:,idx_train])))
+        scores[idx_train,idx_train,:] = scores_
+        clfs.append(clfs_)
+    
+    print('cv different time samples\n')
+    for idx_train,_ in tqdm(enumerate(times[::n_]),desc='off diag'):
+        for idx_test,_ in enumerate(times[::n_]):
+            if idx_train != idx_test:
+                scores_ = []
+                for ii,(train_,test_) in enumerate(cv.split(data)):
+                    clf = clfs[idx_train][ii]
+                    
+                    scores_.append(metrics.roc_auc_score(labels[test_],
+                                            clf.predict(data[test_,:,idx_test])))
+                    
+                scores[idx_test,idx_train,:] = scores_
+    pickle.dump(scores,open('D:\\working_memory\\subject%d.p'%sub,'wb'))
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(12,10))
+    im = ax.imshow(scores.mean(-1),interpolation=None,origin='lower',
+                  cmap='winter',vmin=0.5,vmax=.9,extent=[-100,6000,-100,6000])
+    ax.set(xlabel='Testing Time (ms)',ylabel='Training Time (ms)',
+          title='Temporal Generalization: subject %d, load2 vs load5'%sub)
+    
+    ax.axvline(0,color='k')
+    ax.axhline(0,color='k')
+    cbar=plt.colorbar(im,ax=ax)
+    cbar.ax.set_title('ROC AUC scores')
+    fig.savefig('D:\\working_memory\\working_memory\\results\\sampled_time\\subject_%d_load2load5_generalization_scores.png'%sub,dpi=300)
+
+    scores_mean = np.mean(scores.diagonal(),axis=0)
+    scores_std = np.std(scores.diagonal(),axis=0)
+    plt.close('all')
+    fig,ax = plt.subplots(figsize=(20,6))
+    time_picks = times[::50] * info['sfreq']
+    scores_picks = scores_mean
+    scores_se = (scores_std/2)
+    ax.plot(time_picks,scores_picks,label='scores')
+    ax.fill_between(time_picks,scores_picks-scores_se,scores_picks+scores_se,color='red',alpha=0.5)
+    ax.axhline(0.5,color='k',linestyle='--',label='chance')
+    ax.axvline(0,color='k',linestyle='--')
+    ax.set(xlabel='Time (ms)',ylabel='ROC AUC',xlim=(-100,6000),ylim=(0.35,1.),title='subject_%d_load2load5_decoding_scores'%sub)
+    ax.set(xticks=np.arange(-100,6000,400))
+    ax.legend()
+    fig.savefig('working_memory\\results/sampled_time/subject_%d_load2load5_decoding_scores.png'%sub,dpi=300)
+
+
+    coef = []
+    for clfs_ in clfs:
+        coef_ = [get_coef(clf,'patterns_',inverse_transform=True) for clf in clfs_]
+        coef.append(coef_)
+    coef = np.array(coef)
+    evoked = mne.EvokedArray(np.mean(coef,axis=1).T,info,tmin=times[0])
+    evoked.times = times[::n_]
+    evoked.save('subject_%d_load2load5_patterns-evo.fif'%sub,)
+    del evoked
+
+
+
+
 #for sub in subs:
 #    files = [f for f in os.listdir() if ('suj%d_'%(sub) in f) and ('vhdr' in f)]
 #    _,_,day_ = re.findall('\d+',files[0])
@@ -149,112 +242,3 @@ subs = np.setdiff1d(subs,np.array([24,27]))
 #    evoked.save('subject_%d_load2load5_patterns-evo.fif'%sub,)
 #    del evoked
 #    #evoked.plot_joint(title='patterns')
-
-from tqdm import tqdm
-from sklearn import metrics
-import pickle
-n_ = 50
-for sub in subs:    
-    epoch_fif = [f for f in os.listdir() if ('-epo.fif' in f) and ('suj%d_'%(sub) in f)]    
-    epochs_1 = mne.read_epochs(epoch_fif[0])
-    epochs_2 = mne.read_epochs(epoch_fif[1])
-    epochs = mne.concatenate_epochs([epochs_1,epochs_2])
-    epochs.event_id = {'load_2':2,'load_5':5}
-    epochs.pick_types(eeg=True,eog=False)
-    
-    labels = np.array(epochs.events[:,-1] == 2,dtype=int)
-    data = epochs.get_data()
-    times = epochs.times
-    info = epochs.info
-    del epochs
-    
-    cv = KFold(n_splits=4,shuffle=True,random_state=12345)
-#    c = int(n_/2)
-    c=0
-    size = len(times[c::n_])
-    scores=np.zeros((size,size,4))
-    times_ = times[c::n_]
-    index_iterator = np.arange(len(times))[c::n_]
-    
-    clfs = []
-    print('cv diagonal\n')
-    for kk,idx_train in tqdm(enumerate(index_iterator),desc='diag loop'):
-        scores_ = []
-        clfs_ = []
-        for train_,test_ in cv.split(data):
-            clf = make_clf()
-            last_ = np.arange(idx_train-c,idx_train+c+1)
-#            clf.fit(data[train_,:,idx_train-c:idx_train+c],labels[train_])
-            clf.fit(data[train_,:,idx_train],labels[train_])
-            clfs_.append(clf)
-            last_ = np.arange(idx_train-c,idx_train+c+1)
-#            scores_.append(metrics.roc_auc_score(labels[test_],
-#                                    clf.predict(data[test_,:,idx_train-c:idx_train+c])))
-            scores_.append(metrics.roc_auc_score(labels[test_],
-                                    clf.predict(data[test_,:,idx_train])))
-        scores[kk,kk,:] = scores_
-        clfs.append(clfs_)
-    
-    print('cv different time samples\n')
-    for idx_v,idx_train in tqdm(enumerate(index_iterator),desc='off diag'):
-        for idx_h,idx_test in enumerate(index_iterator):
-            if idx_train != idx_test:
-                scores_ = []
-                for ii,(train_,test_) in enumerate(cv.split(data)):
-                    clf = clfs[idx_v][ii]
-                    
-#                    scores_.append(metrics.roc_auc_score(labels[test_],
-#                                            clf.predict(data[test_,:,idx_test-c:idx_test+c])))
-                    scores_.append(metrics.roc_auc_score(labels[test_],
-                                            clf.predict(data[test_,:,idx_test])))
-                    
-                scores[idx_v,idx_h,:] = scores_
-    pickle.dump(scores,open('D:\\working_memory\\subject%d.p'%sub,'wb'))
-    plt.close('all')
-    fig, ax = plt.subplots(figsize=(12,10))
-    im = ax.imshow(scores.mean(-1),interpolation=None,origin='lower',
-                  cmap='winter',vmin=0.5,vmax=.9,extent=[-100,6000,-100,6000])
-    ax.set(xlabel='Testing Time (ms)',ylabel='Training Time (ms)',
-          title='Temporal Generalization: subject %d, load2 vs load5'%sub)
-
-    ax.axvline(0,color='k')
-    ax.axhline(0,color='k')
-    cbar=plt.colorbar(im,ax=ax)
-    cbar.ax.set_title('ROC AUC scores')
-    fig.savefig('D:\\working_memory\\working_memory\\results\\subject_%d_load2load5_generalization_scores.png'%sub,dpi=300)
-
-    scores_mean = np.mean(scores.diagonal(),axis=0)
-    scores_std = np.std(scores.diagonal(),axis=0)
-    plt.close('all')
-    fig,ax = plt.subplots(figsize=(20,6))
-    time_picks = times_ * info['sfreq']
-    scores_picks = scores_mean
-    scores_se = (scores_std/2)
-    ax.plot(time_picks,scores_picks,label='scores')
-    ax.fill_between(time_picks,scores_picks-scores_se,scores_picks+scores_se,color='red',alpha=0.5)
-    ax.axhline(0.5,color='k',linestyle='--',label='chance')
-    ax.axvline(0,color='k',linestyle='--')
-    ax.set(xlabel='Time (ms)',ylabel='ROC AUC',xlim=(-100,6000),ylim=(0.35,1.),title='subject_%d_load2load5_decoding_scores'%sub)
-    ax.set(xticks=np.arange(-100,6000,400))
-    ax.legend()
-    fig.savefig('working_memory\\results/subject_%d_load2load5_decoding_scores.png'%sub,dpi=300)
-
-
-    coef = []
-    for clfs_ in clfs:
-        coef_ = [get_coef(clf,'patterns_',inverse_transform=True) for clf in clfs_]
-        coef.append(coef_)
-    coef = np.array(coef)
-    if len(coef.shape) > 2:
-        
-        coef = np.swapaxes(coef,0,1)
-        coef = coef.mean(0)
-        coef = np.swapaxes(coef,0,1)
-        coef = coef.reshape(coef.shape[0],-1)
-    else:
-        coef = coef.mean(1)
-        coef = coef.T
-    evoked = mne.EvokedArray(coef,info,tmin=times_[0])
-    evoked.times = times_
-    evoked.save('subject_%d_load2load5_patterns-evo.fif'%sub,)
-    del evoked
