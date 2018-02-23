cb8cc1d8411a3c4bffaeee2f43f491a0a0d04892
Ning Mei <semsociaty@gmail.com>
2018-02-23 11:27:18 -0500

    update
    1. matrix operation, speed up processing
    2. make sure the plots are correct
    3. add comments

--- scripts/measure similarity.py
+++ scripts/measure similarity.py
@@ -26,52 +26,51 @@ epochs_ = glob(os.path.join(working_dir,'*-epo.fif'))
 results = {}
 for e in epochs_:
     epochs = mne.read_epochs(e,preload=True)
+    sub,load,day = re.findall('\d+',e)
+    # due to mne python add additional last sample to the data, we take it out and make it a even number length for the time dimension
     images = epochs.copy().crop(0,4).get_data()[:,:,:4000]
     delay = epochs.copy().crop(4,9.8).get_data()[:,:,:5800]
-    
+    # get the number of trials
     n_trials = images.shape[0]
+    # segmenting the data with a 20 ms nonoverlapping sharp window
     images = images.reshape(n_trials,61,20,-1)
     delay = delay.reshape(n_trials,61,20,-1)
+    # average over the 20 ms window
+    images_ = images.mean(2)
+    delay_ = delay.mean(2)
+    # compute the euclidean distance and cosine distance in matrix operations
     
-    confusion = []
-    for trial in tqdm(range(images.shape[0])):
-        temp_confusion = np.zeros((images.shape[-1],delay.shape[-1]))
-        for ii in range(images.shape[-1]):
-            for jj in range(delay.shape[-1]):
-                temp_confusion[ii,jj] = euclidean_distances(np.mean(images[trial,:,:,ii],axis=1).reshape(1,-1),
-                                                            np.mean(delay[trial,:,:,jj], axis=1).reshape(1,-1))
-        
-        confusion.append(temp_confusion)
-    
-    confusion = np.array(confusion)
-    sub,load,day = re.findall('\d+',e)
-    results['sub%s_load%s_day%s'%(sub,load,day)]={'euclidean_distances':confusion}
-    
-    confusion_mean = confusion.mean(0)
-
+    euclidean,cosine = [],[]
+    for trial in tqdm(range(images.shape[0]),desc='distance'):
+        # euclidean distance
+        temp_confusion = sp.distance.cdist(images_[trial].T,delay_[trial].T,'euclidean')
+        euclidean.append(temp_confusion)
+        # cosine distance
+        # subtract from 1 because the algorithm have cosine distance subtracted from 1,
+        # so subtract it from 1 will give us the value of cosine 
+        temp_confusion = 1 - sp.distance.cdist(images_[trial].T,delay_[trial].T,'cosine')
+        cosine.append(temp_confusion)
+    # covert to numpy array
+    euclidean = np.array(euclidean)
+    cosine = np.array(cosine)
+    # save the result in a dictionary
+    results['sub%s_load%s_day%s'%(sub,load,day)]={'euclidean_distances':euclidean,'cosine_distance':cosine}
+    # take the mean over the trials
+    euclidean_mean = euclidean.mean(0)
+    # plot the mean euclidean distance over trials
     fig,ax = plt.subplots(figsize=(12,12))
-    im = ax.imshow(confusion_mean,origin='lower',aspect='auto',cmap='RdBu_r',extent=[0,6000,0,4000])
+    im = ax.imshow(euclidean_mean,origin='lower',aspect='auto',cmap='RdBu_r',extent=[0,6000,0,4000])
     ax.set(xlabel='Delay',ylabel='Encode',title='Euclidean distance\nsub%s_load%s_day%s'%(sub,load,day))
     plt.colorbar(im)
     fig.savefig(os.path.join(saving_dir,'Euclidean distance_sub%s_load%s_day%s.png'%(sub,load,day)),dpi=300)
-    
-    confusion = []
-    for trial in tqdm(range(images.shape[0])):
-        temp_confusion = np.zeros((images.shape[-1],delay.shape[-1]))
-        for ii in range(images.shape[-1]):
-            for jj in range(delay.shape[-1]):
-                temp_confusion[ii,jj] = 1- sp.distance.cdist(images[trial,:,:,ii],delay[trial,:,:,jj],'cosine')[0,1]
-        
-        confusion.append(temp_confusion)
-    
-    confusion = np.array(confusion)
-    results['sub%s_load%s_day%s'%(sub,load,day)]['cosine_distance']=confusion
-    
+    # plot the mean cosine distance over trials
+    cosine_mean = cosine.mean(0)
     fig,ax = plt.subplots(figsize=(12,12))
-    im = ax.imshow(confusion_mean,origin='lower',aspect='auto',cmap='RdBu_r',extent=[0,6000,0,4000])
+    im = ax.imshow(cosine_mean,origin='lower',aspect='auto',cmap='RdBu_r',extent=[0,6000,0,4000])
     ax.set(xlabel='Delay',ylabel='Encode',title='Cosine distance\nsub%s_load%s_day%s'%(sub,load,day))
     plt.colorbar(im)
     fig.savefig(os.path.join(saving_dir,'Cosine distance_sub%s_load%s_day%s.png'%(sub,load,day)),dpi=300)
 
 pickle.dump(results,open(os.path.join(working_dir,'similarity measure results.p'),'wb')  )  
     
+
