0ba9906c80bab6e32867e49ace24290863500ca0
Ning Mei <semsociaty@gmail.com>
2018-03-06 15:50:44 -0500

    delay - performance

--- scripts/load 5 specialized/train on encodes predict_proba in delay (per trial).py
+++ scripts/load 5 specialized/train on encodes predict_proba in delay (per trial).py
@@ -3,7 +3,8 @@
 Created on Mon Mar  5 18:28:38 2018
 
 @author: ning
-
+decoding the order effect of the encoding period - there is no order effect
+predict performance using encoding period signals
 """
 
 import os
@@ -17,16 +18,22 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import pandas as pd
 from mne.decoding import get_coef
 from sklearn import metrics
-#    from scipy import stats as stats
+from scipy import stats as stats
 import pickle
 import re
 working_dir = 'D:/working_memory/encode_delay_prode_RSA_preprocessing/'
-saving_dir = 'D:/working_memory/working_memory/results/load_5/train_probe_test_encode/train_test_probe_encode/'
+saving_dir = 'D:/working_memory/working_memory/results/order effect/'
 if not os.path.exists(saving_dir):
     os.mkdir(saving_dir)
 from glob import glob
 from tqdm import tqdm
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import StratifiedKFold,permutation_test_score
+from sklearn.multiclass import OneVsOneClassifier,OneVsRestClassifier
+from sklearn.svm import SVC
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from mne.decoding import Vectorizer,LinearModel
+from sklearn.utils import shuffle
 #    from mne.decoding import GeneralizingEstimator
 
 condition = 'load5'
@@ -39,7 +46,7 @@ e_ = event_files[9]
 
 epochs = mne.read_epochs(e,preload=True)
 epochs.resample(100)
-event = pd.read_csv(e_)
+event = epochs.events
 sub,load,day = re.findall('\d+',e)
 # get the order of the stimulu
 trial_orders = pd.read_excel('D:\\working_memory\\working_memory\\EEG Load 5 and 2 Design Fall 2015.xlsx',sheetname='EEG_Load5_WM',header=None)
@@ -48,67 +55,90 @@ trial_orders['target'] = 1- trial_orders['target']
 trial_orders["row"] = np.arange(1,41)
 original_events = pd.read_csv('D:\\working_memory\\signal detection\\suj%s_wml%s_day%s-photo_WM_TS'%(sub,load,day),sep='\t')
 original_events = original_events[np.abs(original_events['TriNo']-80)<5]
+event = pd.DataFrame(event,columns=['tms','e','Comnt'])
 event['trial']=[np.where(original_events['TMS']==time_)[0][0]+1 for time_ in event['tms']]
 working_trial_orders = trial_orders.iloc[event['trial']-1]
+working_events = original_events.iloc[event['trial']-1]
 
 # split data into encode, delay, and probe
 images = epochs.copy().crop(-16,-6).get_data()[:,:,:1000]# all 5 images - 10 seconds
 delay = epochs.copy().crop(-6,0).get_data()[:,:,:600]# delay - 6 seconds
 probe = epochs.copy().crop(0,2).get_data()[:,:,:200]# probe - 2 seconds
-labels = np.arange(5)
-clfs = []
-preds = []
-for trial in tqdm(range(images.shape[0]),desc=''):
-    X_train = images[trial].reshape(5,61,200)
-    X_test = delay[trial]
-    clfs_ = [make_clf(vectorized=False,voting='linear',decoding=False).fit(X_train[:,:,ii],
-             labels) for ii in range(X_train.shape[-1])]
-    preds_ = []
-    for clf in clfs_:
-        preds_.append([clf.predict_proba(X_test[:,ii].reshape(1,-1)) for ii in range(X_test.shape[-1])])
-    preds.append(preds_)
-preds = np.array(preds)
-preds = preds.reshape(preds.shape[0],preds.shape[1],preds.shape[2],preds.shape[-1])
-preds_max = preds.argmax(-1)
 
-cmap = plt.cm.tab10
-bounds = [0,1,2,3,4,5]
-norm = colors.BoundaryNorm(bounds,cmap.N)
-im=plt.imshow(preds_max.mean(0),cmap=cmap,origin='lower',
-              aspect='auto',extent=[0,6000,0,2000],norm=norm)
-plt.colorbar(im,boundaries=bounds,norm=norm,cmap=cmap,ticks=np.arange(1,6))
+# prepare train-test data in the encodeing period
+#X = np.concatenate(np.split(images,5,axis=-1),axis=0)# this is the correct way to stack the data
+X = delay
+Comnt_dict = {'Correct Rejection':1,'Hit':1,'False Alarm':0,'Miss':0,'No Response':0}
+labels = working_events['Comnt'].map(Comnt_dict)
+cv = StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+clf = []
+clf.append(('Vec',Vectorizer()))
+clf.append(('ST',StandardScaler()))
+clf.append(('EST',SVC(random_state=12345,kernel='linear',class_weight='balanced')))
+clf = Pipeline(clf,)
 
 
-plt.imshow(preds_max.std(0),origin='lower',aspect='auto',vmin=.8);plt.colorbar()
+res = [[permutation_test_score(clf,X[:,:,ii],labels,cv=cv,n_permutations=int(5e2),verbose=1,n_jobs=4)]for ii in range(X.shape[-1])]
 
-clf = make_clf(vectorized=True,voting='linear',decoding=False)
-X = images.reshape(images.shape[0]*5,61,200)
-labels = np.tile(np.arange(5),images.shape[0])
-a,b,c = 
+def ST(X):
+    return (X - X.mean(0))/X.std(0)
+def Comnt_dict(x):
+    if x == 'Correct Rejection':
+        return 1
+    elif x == 'Hit':
+        return 1
+    elif x == 'False Alarm':
+        return 0
+    elif x == 'Miss':
+        return 0
+    else:
+        return 0
+
+X = np.concatenate([ST(mne.read_epochs(e).resample(100,n_jobs=4).copy().crop(-6,0).get_data()[:,:,:600]) for e in epoch_files[:-4]],axis=0)
+X=[]
+labels = []
+for e,e_ in zip(epoch_files[:-4],event_files[:-4]):
+    epochs = mne.read_epochs(e,preload=True)
+    epochs.resample(100,n_jobs=4)
+    xx = stats.zscore(epochs.copy().crop(-6,0).get_data()[:,:,:600],axis=0,ddof=1)
+    X.append(xx)
+    event = epochs.events
+    sub,load,day = re.findall('\d+',e)
+    # get the order of the stimulu
+    trial_orders = pd.read_excel('D:\\working_memory\\working_memory\\EEG Load 5 and 2 Design Fall 2015.xlsx',sheetname='EEG_Load5_WM',header=None)
+    trial_orders.columns = ['load','image1','image2','image3','image4','image5','target','probe']
+    trial_orders['target'] = 1- trial_orders['target']
+    trial_orders["row"] = np.arange(1,41)
+    original_events = pd.read_csv('D:\\working_memory\\signal detection\\suj%s_wml%s_day%s-photo_WM_TS'%(sub,load,day),sep='\t')
+    original_events = original_events[np.abs(original_events['TriNo']-80)<5]
+    if original_events.shape == (0,6):
+        original_events = pd.read_csv('D:\\working_memory\\signal detection\\suj%s_wml%s_day%s-photo_WM_TS'%(sub,load,day),sep='\t')
+        original_events = original_events[original_events['TriNo']==8]
+    event = pd.DataFrame(event,columns=['tms','e','Comnt'])
+    event['trial']=[np.where(original_events['TMS']==time_)[0][0]+1 for time_ in event['tms']]
+    working_trial_orders = trial_orders.iloc[event['trial']-1]
+    working_events = original_events.iloc[event['trial']-1]
+    
+    labels_ = working_events['Comnt'].apply(Comnt_dict)
+    labels.append(labels_)
+labels = np.concatenate(labels).astype(int)
+X = np.concatenate(X).astype(np.float32)
+
+cv = StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+clf = []
+clf.append(('Vec',Vectorizer()))
+clf.append(('ST',StandardScaler()))
+clf.append(('EST',SVC(random_state=12345,kernel='linear',class_weight='balanced')))
+clf = Pipeline(clf,)
+a,b,c = permutation_test_score(clf,X,labels,cv=cv,n_permutations=int(5e2),verbose=1,n_jobs=4)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+cv = StratifiedKFold(n_splits=5,shuffle=True,random_state=12345)
+clf = []
+clf.append(('ST',StandardScaler()))
+clf.append(('EST',SVC(random_state=12345,kernel='linear',class_weight='balanced')))
+clf = Pipeline(clf,)
+res = [[permutation_test_score(clf,X[:,:,ii],labels,cv=cv,n_permutations=int(5e2),verbose=1,n_jobs=4)]for ii in range(X.shape[-1])]
 
 
 
